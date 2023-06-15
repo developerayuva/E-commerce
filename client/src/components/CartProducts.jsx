291de5596382ad14c4f45eadebab19a1e445ba4a
developerayuva <ayushvatsa1810@gmail.com>
2023-06-15 23:23:25 +0530

    Handled empty cart case

--- client/src/components/CartProducts.jsx
+++ client/src/components/CartProducts.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from "react";
 import CartContext from "../context/Cart/CartContext"
 import Item from "./Item";
+import EmptyCartImg from "./ImageComponents/EmptyCartImg";
 
 function CartProducts() {
     const contextCart = useContext(CartContext);
@@ -14,9 +15,13 @@ function CartProducts() {
     return (
         <div className="container">
             <div className="my-5 row">
-                {cartItems.map((item) => {
+                {cartItems.length!==0 ? cartItems.map((item) => {
                     return <Item key={item._id} item={item} insideCart={true}/>
-                })}
+                }):
+                <div className="w-50 mx-auto">
+                    <EmptyCartImg />
+                </div>
+                }
             </div>
         </div>
     )
---
+++ client/src/components/ImageComponents/EmptyCartImg.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+//Image taken from vecteezy.com
+import emptyCartImg from "../../images/Empty_cart.png";
+
+function EmptyCartImg() {
+    return (
+        <img src={emptyCartImg} alt="about-us-img" className="img-class"/>
+    )
+}
+
+export default EmptyCartImg;
